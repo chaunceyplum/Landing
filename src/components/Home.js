b4ff33f91c9fc352ba125bb00d902f5a4f8d158d
Chauncey <Chaunceyplum@gmail.com>
2023-02-20 22:28:05 -0500

    adding some more small animations, also added more stuff to homescreen, gained more of an understanding of react-spring, used acync animations here

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { animated, useSpring, useScroll } from '@react-spring/web'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
@@ -7,12 +7,30 @@ import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
-
+import pic1 from '../images/pic1.svg'
 const Home = () => {
+  // const springs = useSpring({
+  //   from: { opacity: 0, marginLeft: -500 },
+  //   to: { opacity: 1, marginLeft: 0 },
+  //   config: { delay: 700, duration: 700 },
+  // })
   const springs = useSpring({
-    from: { opacity: 0, marginLeft: -500 },
-    to: { opacity: 1, marginLeft: 0 },
-    config: { delay: 1000, duration: 1000 },
+    from: { x: 0, y: 0 },
+    to: async (next, cancel) => {
+      // await next({ fontSize: '3em' })
+      // await next({ fontSize: '1em' })
+      await next({ x: 80, y: 20 })
+      await next({ x: 0, y: 0 })
+      await next({ x: -80, y: 20 })
+      await next({ x: 0, y: 0 })
+    },
+
+    loop: true,
+    config: {
+      mass: 5,
+      friction: 120,
+      tension: 120,
+    },
   })
   const alignCenter = { display: 'flex', alignItems: 'center' }
   //const { scrollYProgress } = useScroll()
@@ -20,15 +38,16 @@ const Home = () => {
   return (
     <div className=''>
       <div className='homeDiv'>
-        <Parallax pages={6} style={{}}>
+        <Parallax pages={7} style={{}}>
           <ParallaxLayer offset={0} speed={1.5}>
             <Container className='homeMainContent'>
               <Row>
-                <Col xs={10} md={8}>
+                <Col xs={12} md={8}>
                   <animated.div style={{ ...springs }}>
                     <h1 className='redText'>
                       <strong>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Lorem ipsum <i>dolor sit amet,</i> consectetur
+                        adipiscing elit.
                       </strong>
                     </h1>
 
@@ -38,15 +57,21 @@ const Home = () => {
                       laboris nisi ut aliquip ex ea commodo consequat.
                     </h4>
                     <br />
-                    <Button className='beigebg btn '>
-                      <strong>Learn More</strong>
+                    <Button className='beigebg btn redText'>
+                      <strong className='redText'>Learn More</strong>
                     </Button>
                   </animated.div>
                 </Col>
-                <Col xs={10} lg={4}>
-                  <animated.div style={{ ...springs }}>
-                    <div className='text-white'>blah</div>
-                  </animated.div>
+                <Col xs={12} md={4}>
+                  {/* <Container>
+                    <Row>
+                      <Col xs={10}>
+                        <div className='center'>
+                          <img src={pic1} alt={pic1} />
+                        </div>
+                      </Col>
+                    </Row>
+                  </Container> */}
                 </Col>
               </Row>
             </Container>
@@ -111,9 +136,9 @@ const Home = () => {
 
                     <Container>
                       <Row>
-                        <Col xs={12} md={6}>
+                        <Col xs={12} lg={6}>
                           <Container className='redBg'>
-                            <animated.div style={{ ...springs }}>
+                            <animated.div style={{}}>
                               <h1>Aenean vel elit scelerisque mauris.</h1>
                               <br />
                               <h6>Venenatis tellus in metus vulputate.</h6>
@@ -128,7 +153,7 @@ const Home = () => {
                           </Container>
                         </Col>
 
-                        <Col xs={12} md={6}>
+                        <Col xs={12} lg={6}>
                           <Container className='redBg'>
                             <h1>Aenean vel elit scelerisque mauris.</h1>
                             <br />
@@ -143,7 +168,7 @@ const Home = () => {
                           </Container>
                         </Col>
 
-                        <Col xs={12} md={6}>
+                        <Col xs={12} lg={6}>
                           <Container className='redBg'>
                             <h1>Aenean vel elit scelerisque mauris.</h1>
                             <br />
@@ -158,7 +183,7 @@ const Home = () => {
                           </Container>
                         </Col>
 
-                        <Col xs={12} md={6}>
+                        <Col xs={12} lg={6}>
                           <Container className='redBg'>
                             <h1>Aenean vel elit scelerisque mauris.</h1>
                             <br />
@@ -211,7 +236,7 @@ const Home = () => {
                 {/* <Col xs={2} /> */}
                 <Col xs={12} sm={6}>
                   <Container className='redBg'>
-                    <div className='text-white'>
+                    <div className='beigeText'>
                       <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore
@@ -278,7 +303,9 @@ const Home = () => {
           </ParallaxLayer>
 
           <ParallaxLayer
-            sticky={{ start: 1, end: 3 }}
+            sticky={{ start: 1, end: 2 }}
+            offset={{ start: 1, end: 2 }}
+            speed={1}
             style={{ ...alignCenter, justifyContent: 'flex-center' }}
             className='homeBackDiv'
           >
@@ -287,41 +314,43 @@ const Home = () => {
                 <Col />
                 <Col xs={0} sm={4}>
                   <Container className='beigeBg'>
-                    <animated.div style={{ ...springs }}>
+                    <animated.div style={{}}>
                       <div className='beigeBg'>
-                        <div>
-                          <h3 className='redText'>
-                            Porta nibh venenatis cras sed felis eget velit
-                            aliquet sagittis. Pellentesque nec nam aliquam sem
-                            et tortor consequat id.
-                          </h3>
+                        <Container>
+                          <div>
+                            <h3 className='redText'>
+                              Porta nibh venenatis cras sed felis eget velit
+                              aliquet sagittis. Pellentesque nec nam aliquam sem
+                              et tortor consequat id.
+                            </h3>
 
-                          <br />
-                          <h6 className='darkText'>
-                            Mattis enim ut tellus elementum sagittis vitae. Ut
-                            consequat semper viverra nam libero. Elementum eu
-                            facilisis sed odio morbi.
-                          </h6>
+                            <br />
+                            <h6 className='darkText'>
+                              Mattis enim ut tellus elementum sagittis vitae. Ut
+                              consequat semper viverra nam libero. Elementum eu
+                              facilisis sed odio morbi.
+                            </h6>
 
-                          <br />
-                          <p>
-                            Porta nibh venenatis cras sed felis eget velit
-                            aliquet sagittis. Pellentesque nec nam aliquam sem
-                            et tortor consequat id. Lectus magna fringilla urna
-                            porttitor rhoncus dolor purus. Malesuada fames ac
-                            turpis egestas sed tempus urna. Sit amet risus
-                            nullam eget felis eget nunc lobortis. Viverra ipsum
-                            nunc aliquet bibendum enim. Lacus suspendisse
-                            faucibus interdum posuere lorem ipsum. Morbi quis
-                            commodo odio aenean sed adipiscing diam donec.
-                            Aliquam purus sit amet luctus venenatis lectus.
-                            Venenatis urna cursus eget nunc scelerisque viverra
-                            mauris. Tincidunt nunc pulvinar sapien et ligula
-                            ullamcorper malesuada. Erat pellentesque adipiscing
-                            commodo elit at imperdiet. Gravida arcu ac tortor
-                            dignissim.
-                          </p>
-                        </div>
+                            <br />
+                            <p>
+                              Porta nibh venenatis cras sed felis eget velit
+                              aliquet sagittis. Pellentesque nec nam aliquam sem
+                              et tortor consequat id. Lectus magna fringilla
+                              urna porttitor rhoncus dolor purus. Malesuada
+                              fames ac turpis egestas sed tempus urna. Sit amet
+                              risus nullam eget felis eget nunc lobortis.
+                              Viverra ipsum nunc aliquet bibendum enim. Lacus
+                              suspendisse faucibus interdum posuere lorem ipsum.
+                              Morbi quis commodo odio aenean sed adipiscing diam
+                              donec. Aliquam purus sit amet luctus venenatis
+                              lectus. Venenatis urna cursus eget nunc
+                              scelerisque viverra mauris. Tincidunt nunc
+                              pulvinar sapien et ligula ullamcorper malesuada.
+                              Erat pellentesque adipiscing commodo elit at
+                              imperdiet. Gravida arcu ac tortor dignissim.
+                            </p>
+                          </div>
+                        </Container>
                       </div>
                     </animated.div>
                   </Container>
@@ -332,19 +361,40 @@ const Home = () => {
               </Row>
             </Container>
           </ParallaxLayer>
+          <ParallaxLayer offset={3} speed={-3.5}>
+            <Container className=''>
+              <Row className='center'>
+                <Col xs={10} lg={12} className='text-center'>
+                  <Container>
+                    <div className='center'>
+                      <img src={pic1} alt={pic1} />
+                    </div>
+                  </Container>
+                </Col>
+              </Row>
+            </Container>
+          </ParallaxLayer>
           <ParallaxLayer offset={4} speed={1.5}>
             <Container className='homeMainContent'>
               <Row className='center'>
                 <Col xs={12} md={10} className='text-center'>
                   <h1 className='redText'>
-                    <strong>Amet massa vitae tortor condimentum.</strong>
+                    <strong>
+                      <em>
+                        Hendrerit gravida rutrum <i>quisque non tellus.</i> Amet
+                        massa vitae tortor condimentum.
+                      </em>
+                    </strong>
                   </h1>
 
                   <br />
-                  <h4 className='beigeText'>
+                  <h3 className='beigeText'>
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat.
-                  </h4>
+                    laboris nisi ut aliquip ex ea commodo consequat.Vel risus
+                    commodo viverra maecenas accumsan lacus vel facilisis.
+                    Imperdiet sed euismod nisi porta lorem mollis aliquam ut
+                    porttitor.
+                  </h3>
                   <br />
 
                   <br />
@@ -356,7 +406,10 @@ const Home = () => {
                     pharetra vel turpis nunc eget. Ultricies integer quis auctor
                     elit sed vulputate mi sit amet. Amet massa vitae tortor
                     condimentum.Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit.
+                    adipiscing elit.Malesuada fames ac turpis egestas sed tempus
+                    urna. Sit amet risus nullam eget felis eget nunc lobortis.
+                    Viverra ipsum nunc aliquet bibendum enim. Lacus suspendisse
+                    faucibus interdum posuere lorem ipsum.
                   </p>
                   {/* <Button className='beigebg btn '>
                       <strong>Learn More</strong>
@@ -375,64 +428,88 @@ const Home = () => {
                 <Col xs={12} md={10} className='text-center'>
                   <Container>
                     <Row>
-                      <Col xs={12} md={6}>
+                      <Col xs={12} md={6} lg={3}>
                         <Container className='redBg'>
-                          <animated.div style={{ ...springs }}>
-                            <h1>Aenean vel elit scelerisque mauris.</h1>
+                          <animated.div style={{}}>
+                            <h1>
+                              Bibendum at varius vel pharetra vel turpis nunc
+                              eget.
+                            </h1>
                             <br />
-                            <h6>Venenatis tellus in metus vulputate.</h6>
+                            <h6>
+                              Lacus suspendisse faucibus interdum posuere lorem
+                              ipsum.
+                            </h6>
                             <br />
                             <br />
                             <p>
-                              Lorem ipsum dolor sit amet, consectetur adipiscing
-                              elit, sed do eiusmod tempor incididunt ut labore
-                              et dolore magna aliqua.
+                              Lectus magna fringilla urna porttitor rhoncus
+                              dolor purus. Malesuada fames ac turpis egestas sed
+                              tempus urna.
                             </p>
                           </animated.div>
                         </Container>
                       </Col>
 
-                      <Col xs={12} md={6}>
+                      <Col xs={12} md={6} lg={3}>
                         <Container className='redBg'>
-                          <h1>Aenean vel elit scelerisque mauris.</h1>
+                          <h1>
+                            Bibendum at varius vel pharetra vel turpis nunc
+                            eget.
+                          </h1>
                           <br />
-                          <h6>Venenatis tellus in metus vulputate.</h6>
+                          <h6>
+                            Lacus suspendisse faucibus interdum posuere lorem
+                            ipsum.
+                          </h6>
                           <br />
                           <br />
                           <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua.
+                            Lectus magna fringilla urna porttitor rhoncus dolor
+                            purus. Malesuada fames ac turpis egestas sed tempus
+                            urna.
                           </p>
                         </Container>
                       </Col>
 
-                      <Col xs={12} md={6}>
+                      <Col xs={12} md={6} lg={3}>
                         <Container className='redBg'>
-                          <h1>Aenean vel elit scelerisque mauris.</h1>
+                          <h1>
+                            Bibendum at varius vel pharetra vel turpis nunc
+                            eget.
+                          </h1>
                           <br />
-                          <h6>Venenatis tellus in metus vulputate.</h6>
+                          <h6>
+                            Lacus suspendisse faucibus interdum posuere lorem
+                            ipsum.
+                          </h6>
                           <br />
                           <br />
                           <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua.
+                            Lectus magna fringilla urna porttitor rhoncus dolor
+                            purus. Malesuada fames ac turpis egestas sed tempus
+                            urna.
                           </p>
                         </Container>
                       </Col>
 
-                      <Col xs={12} md={6}>
+                      <Col xs={12} md={6} lg={3}>
                         <Container className='redBg'>
-                          <h1>Aenean vel elit scelerisque mauris.</h1>
+                          <h1>
+                            Bibendum at varius vel pharetra vel turpis nunc
+                            eget.
+                          </h1>
                           <br />
-                          <h6>Venenatis tellus in metus vulputate.</h6>
+                          <h6>
+                            Lacus suspendisse faucibus interdum posuere lorem
+                            ipsum.
+                          </h6>
                           <br />
                           <br />
                           <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua.
+                            Lectus magna fringilla urna porttitor rhoncus dolor
+                            purus. Malesuada fames ac turpis egestas sed tempus
+                            urna.
                           </p>
                         </Container>
                       </Col>
